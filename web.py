fbc7a78a929aaa7676a8c404fa37300b3e66bbda
zibokapi <zibokapi@gmail.com>
2023-09-23 15:46:12 +0800

    refactor

--- web.py
+++ web.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python3
 import os
-from typing import Dict
+from typing import Dict, Union
 
 from flask import Flask, render_template, request, redirect, url_for
-from github import Github, Issue, Auth
+from github import Github, Issue, Auth, Repository, AuthenticatedUser
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,19 +11,12 @@ load_dotenv()
 app = Flask(__name__)
 g = Github()
 gt = os.getenv("GT", None)
-loggedin = False
-cache: Dict[str, Issue.Issue] = {}
+cache: Dict[str, Union[Issue.Issue, Repository.Repository, AuthenticatedUser.AuthenticatedUser]] = {}
 
 def site_router():
-    try: 
-        g.get_user().login
-    except: 
-        return redirect(url_for("index"))
-    if not cache: 
-        return redirect(url_for("welcome"))
-    else: 
-        return None
-            
+    if "user" not in cache: return redirect(url_for("index"))
+    elif len(cache) == 1: return redirect(url_for("welcome"))
+    else: return None
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -31,8 +24,9 @@ def index():
         try:
             github_token = request.form.get("input_data")
             auth = Auth.Token(gt) if github_token == "test" else Auth.Token(github_token)
-            g.__init__(auth=auth)
-            g.get_user().login
+            user = g.__init__(auth=auth).get_user()
+            cache["user"] = user
+            user.login # TODO check if actually needed
             return redirect(url_for("welcome"))
         except Exception as e:
             return render_template("home.html", error_message=str(e))
@@ -40,19 +34,22 @@ def index():
 
 @app.route("/welcome", methods=["GET", "POST"])
 def welcome():
-    try: g.get_user().login
-    except: return redirect(url_for("index"))
+    if (c := site_router()) is not None: return c
     if request.method == "POST":
         repo_name = request.form["repo_name"]
-        repo_name = "geohotstan/SolidGoldMagikarp" if repo_name == "test" else repo_name
-        return redirect(url_for("get_bounty_issues", repo_name=repo_name.replace("/", "_")))
+        if repo_name == "test": repo_name = "magikarpusesplash/SolidGoldMagikarp"  
+        try: 
+            repo = g.get_repo(repo_name)
+            cache['repo'] = repo
+        except Exception as e:
+            return render_template("index.html", error_message=str(e) + "\n" + "invalid repository")
+        return redirect(url_for("get_bounty_issues", repo_name=repo_name.replace("/", "-")))
     return render_template("index.html")
 
 @app.route("/issues/<repo_name>")
 def get_bounty_issues(repo_name: str):
-    try: g.get_user().login
-    except: return redirect(url_for("index"))
-    repo = g.get_repo(repo_name.replace("_", "/"))
+    if (c := site_router()) is not None: return c
+    repo = cache['repo']
     issues = [issue for issue in repo.get_issues(labels=["bounty"])]
     for i in issues: cache[str(i.number)] = i
     return render_template("issues.html", repo_name=repo_name, issues=issues)
@@ -66,7 +63,17 @@ def view_issue(repo_name: str, issue_number: int):
 @app.route("/issue<repo_name>/<issue_number>/start")
 def start_function(repo_name, issue_number):
     if (c := site_router()) is not None: return c
-    # fork the repo under a fork name bounty/<name>
+    # automatically fork the repo using repo_name
+    user, repo = cache['user'], cache['repo']
+    g.get_user()
+    repo.create_fork()
+
+    # git clone https://github.com/your-username/repo_name.git
+
+    # git checkout -b bounty/<branch-name>
+
+    # git push origin bounty/<branch-name>
+
     return None
 
 if __name__ == "__main__":
